dac21ff34efb9f7fee7287f5f55a1748681cf145
Imants Liepins <iliepins01@gmail.com>
2021-05-27 07:24:12 +0300

    skolu kontrolieris un skati

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LessonsController;
+use App\Http\Controllers\SchoolController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,7 @@ Route::get('/', function () {
 });
 
 Route::resource('lessons', LessonsController::class);
+Route::resource('schools', SchoolController::class);
 
 Auth::routes();
 
