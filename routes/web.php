aa419aa73554f6d18413a5007e2ea042ec7549b1
Imants Liepins <iliepins01@gmail.com>
2021-05-27 07:15:47 +0300

    nodarbību kontrolieris un skatu faili

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LessonsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('lessons', LessonsController::class);
 
 Auth::routes();
 
