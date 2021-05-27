f417c82224a08e6c8db82b22aa2fdddeabc0241c
Daniels <daniels.dam1001@gmail.com>
2021-05-27 13:00:54 +0300

    Skolotāja nodarbību skats

--- database/seeders/LessonSeeder.php
+++ database/seeders/LessonSeeder.php
@@ -14,19 +14,20 @@ class LessonSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('files')->insert([
-            'name' => 'Pirmais fails',
-            'url' => 'https://www.youtube.com/embed/w4a2ge9N31E',
-            'poster_url' => 'Purla/uz/bildi',
-        ]);
+        // DB::table('files')->insert([
+        //     'name' => 'Pirmais fails',
+        //     'url' => 'https://www.youtube.com/embed/w4a2ge9N31E',
+        //     'poster_url' => 'Purla/uz/bildi',
+        // ]);
 
         DB::table('lessons')->insert([
             'school_id' => 1,
             'author_id' => 4,
             'file_id' => 1,
-            'title' => 'Pirmā nodarbība',
-            'description' => 'Šī ir pirmā testa nodarbība. Bliežam!',
+            'title' => 'Otrā nodarbība',
+            'description' => 'Šī ir otrā testa nodarbība. Bliežam!',
             'poster_url' => 'https://i.ibb.co/1QF7bhw/missing-picture.jpg',
+            'created_at' => date('Y-m-d H:i:s'),
         ]);
     }
 }
